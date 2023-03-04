f06b9ac9d2d952d05c12e6c82d42693f69df7bf3
Ezio <37951898+ezio416@users.noreply.github.com>
2023-03-04 13:23:45 -0700

    +Storage

---
+++ Storage.cs
@@ -0,0 +1,11 @@
+// c 2023-01-19
+// m 2023-03-04
+
+namespace TMT {
+    class Storage {
+        public static Maps.CampaignMap[] campaignMaps;
+        public static Maps.MyMap[] myMaps;
+        public static Accounts.Account[] myMapsAccounts;
+        public static Records.Record[] myMapsRecentRecords;
+    }
+}
