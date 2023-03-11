9d5b556aa8621370f32fe3521f90ebbac93b175c
Ezio <37951898+ezio416@users.noreply.github.com>
2023-03-10 22:37:20 -0700

    fix bad upload timestamps

--- Models/Map.cs
+++ Models/Map.cs
@@ -1,11 +1,12 @@
 // c 2023-03-09
-// m 2023-03-09
+// m 2023-03-10
 
 namespace TMT.Models {
     public abstract class Map {
         public string authorName { get; set; }
         [JsonPropertyName("author")] public string authorId { get; set; }
         public abstract float authorTime { get; set; }
+        public bool badUploadTime { get; set; }
         public abstract float bronzeTime { get; set; }
         public abstract Uri downloadUrl { get; set; }
         public abstract float goldTime { get; set; }
@@ -14,9 +15,10 @@ namespace TMT.Models {
         [JsonPropertyName("uid")] public abstract string mapUid { get; set; }
         [JsonPropertyName("medal")] public int personalMedal { get; set; }
         public float personalTime { get; set; }
-        public Records.Record[] records { get; set; }
+        public List<Records.Record> records { get; set; }
         public abstract float silverTime { get; set; }
         public Uri thumbnailUrl { get; set; }
+        [JsonPropertyName("updateTimestamp")] public int updatedUnix { get; set; }
         public abstract string uploadedIsoUtc { get; set; }
         [JsonPropertyName("uploadTimestamp")] public int uploadedUnix { get; set; }
     }
