8259b4fd59d31f825b4947d98cbf2a7fbd9e61d9
Ezio <37951898+ezio416@users.noreply.github.com>
2023-03-10 17:31:13 -0700

    fix for Android

--- Core/Auth.cs
+++ Core/Auth.cs
@@ -1,5 +1,5 @@
 // c 2023-01-10
-// m 2023-03-06
+// m 2023-03-10
 
 namespace TMT.Core {
     class Auth {
@@ -7,16 +7,15 @@ namespace TMT.Core {
         private record class _Token(string accessToken);
 
 
-        static object[] _clients;
+        static HttpClient[] _clients;
         public static async Task<HttpClient[]> GetClients() {
             if (_clients != null)
-                return (HttpClient[])_clients;
+                return _clients;
 
             using HttpClient _client0 = new();
             HttpClient client1 = new();
             HttpClient client2 = new();
 
-            using StringContent defaultContent = new("", Encoding.UTF8, "application/json");
             _client0.BaseAddress = new Uri("https://public-ubiservices.ubi.com/v3/profiles/sessions/");
             client1.BaseAddress = new Uri("https://prod.trackmania.core.nadeo.online/");
             client2.BaseAddress = new Uri("https://live-services.trackmania.nadeo.live/");
@@ -26,30 +25,35 @@ namespace TMT.Core {
             _client0.DefaultRequestHeaders.Add("Authorization", basic);
             _client0.DefaultRequestHeaders.Add("Ubi-AppId", "86263886-327a-4328-ac69-527f0d20a237");
             _client0.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", $"{Config.api.agent} / {Config.api.username}");
-            using HttpResponseMessage response = await _client0.PostAsync("", defaultContent);
+            using HttpResponseMessage response = await _client0.PostAsync("", new StringContent("", Encoding.UTF8, "application/json"));
             string responseString = await response.Content.ReadAsStringAsync();
             string token0 = "ubi_v1 t=" + JsonSerializer.Deserialize<_Ticket>(responseString).ticket;
 
             // using L0 to get L1
             client1.DefaultRequestHeaders.Add("Authorization", token0);
             Various.ApiWait();
-            using HttpResponseMessage response1 = await client1.PostAsync("v2/authentication/token/ubiservices", defaultContent);
+            using HttpResponseMessage response1 = await client1.PostAsync(
+                "v2/authentication/token/ubiservices",
+                new StringContent("", Encoding.UTF8, "application/json")
+            );
             responseString = await response1.Content.ReadAsStringAsync();
             string token1 = "nadeo_v1 t=" + JsonSerializer.Deserialize<_Token>(responseString).accessToken;
 
             // using L1 to get L2
             client1.DefaultRequestHeaders.Clear();
             client1.DefaultRequestHeaders.Add("Authorization", token1);
-            using FormUrlEncodedContent paramContent = new(new[] { new KeyValuePair<string, string>("audience", "NadeoLiveServices") });
             Various.ApiWait();
-            using HttpResponseMessage response2 = await client1.PostAsync("v2/authentication/token/nadeoservices", paramContent);
+            using HttpResponseMessage response2 = await client1.PostAsync(
+                "v2/authentication/token/nadeoservices",
+                new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>("audience", "NadeoLiveServices") })
+            );
             responseString = await response2.Content.ReadAsStringAsync();
             string token2 = "nadeo_v1 t=" + JsonSerializer.Deserialize<_Token>(responseString).accessToken;
 
             client2.DefaultRequestHeaders.Add("Authorization", token2);
 
             _clients = new[] { client1, client2 };
-            return (HttpClient[])_clients;
+            return _clients;
         }
     }
 }
